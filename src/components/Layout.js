c473a2d379743eac30c2f1a578a1ad0f57a814ca
Felix Lai <felix.lai@hotmail.com>
2022-11-29 10:20:43 -0500

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,46 +1,48 @@
-import React, { Suspense } from 'react';
-import 'antd/dist/antd.css';
-import { fetchSources } from '../api/newsAPI';
+import React, { Suspense } from "react";
+import "antd/dist/antd.css";
+import { fetchSources } from "../api/newsAPI";
 
-const NewsSection = React.lazy(() => import('./NewsSection'));
-const News = React.lazy(() => import('./News'));
+const NewsSection = React.lazy(() => import("./NewsSection"));
+const News = React.lazy(() => import("./News"));
 
 const { Header, Sider, Content } = Layout;
 
 class MainLayout extends React.Component {
-    state = {
-        collapsed: false,
-        sources: [],
-        homePage: true,
-        query: '',
-    };
+  state = {
+    collapsed: false,
+    sources: [],
+    homePage: true,
+    query: "",
+  };
 
-    toggle = () => {
-        this.setState({
-          collapsed: !this.state.collapsed,
-        });
-      };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
 
-      async componentDidMount() {
-        const sources = await fetchSources();
-    
-        this.setState({ sources });
-      }
+  async componentDidMount() {
+    const sources = await fetchSources();
 
-      loadNews(query) {
-        this.setState({
-          homePage: false,
-          query: query
-        });
-      }
+    this.setState({ sources });
+  }
 
-      render() {
-          return (
-              <Layout>
-                  <Sider trigger={null} collapsible collapsed={!this.state.collapsed}>
-                      
-                  </Sider>
-              </Layout>
-          )
-      }
+  loadNews(query) {
+    this.setState({
+      homePage: false,
+      query: query,
+    });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={!this.state.collapsed}>
+          <div className="logo">
+            <h2>{!this.state.collapsed ? "N" : "Newsio"}</h2>
+          </div>
+        </Sider>
+      </Layout>
+    );
+  }
 }
