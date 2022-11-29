f941653fba02a0626b36d61d1b8b393c702ef1e2
Felix Lai <felix.lai@hotmail.com>
2022-11-29 10:52:57 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import MainLayout from './components/Layout';
 
-ReactDOM.render(<MainLayout />, document.getElementById('container'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MainLayout />
+  </React.StrictMode>
+);
