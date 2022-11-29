a474c66aeb9f7a875de47e3c035e0bdcd08814c4
Felix Lai <felix.lai@hotmail.com>
2022-11-29 09:48:17 -0500

    Update NewsSection.js

---
+++ src/components/NewsSection.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import 'antd/dist/antd.css';
+import { Row, Col, Divider, Card } from 'antd';
+import { fetchNews } from '../api';
+import ReactHtmlParser from 'react-html-parser';
+
+const NewsSection = (request) => {
+    const [newsSection, setNewsSection] = useState([]);
+}
+
+export default NewsSection;
