bb9c836ffe2f17b7d542a1973ca74686004a682b
Felix Lai <felix.lai@hotmail.com>
2022-11-29 10:12:21 -0500

    Update NewsSection.js

--- src/components/NewsSection.js
+++ src/components/NewsSection.js
@@ -1,56 +1,70 @@
-import React, { useState, useEffect } from 'react'
-import 'antd/dist/antd.css';
-import { Row, Col, Divider, Card } from 'antd';
-import { fetchNews } from '../api/newsAPI';
-import ReactHtmlParser from 'react-html-parser';
+import React, { useState, useEffect } from "react";
+import "antd/dist/antd.css";
+import { Row, Col, Divider, Card } from "antd";
+import { fetchNews } from "../api/newsAPI";
+import ReactHtmlParser from "react-html-parser";
 
 const NewsSection = (request) => {
-    const [newsSection, setNewsSection] = useState([]);
-    useEffect(() => {
-        const fetchAPI = async () => {
-            setNewsSection(await fetchNews(request));
-        };
-        fetchAPI();
-    }, [request]);
+  const [newsSection, setNewsSection] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setNewsSection(await fetchNews(request));
+    };
+    fetchAPI();
+  }, [request]);
 
-    return (
-        <div>
-            <Row>
-                <Col>
-                <h1 style={{ fontSize: '30px' }}>{request.topHeading}</h1>
-                </Col>
-            </Row>
-            <Row>
-                {newsSection.length > 1 ?
-                    newsSection.map((article, key) => article.urlToImage === "" || article.urlToImage === null ? null :
-                    <Col key={key} md={{ span: 8 }} sm={{ span: 24 }}>
-                        <Card onClick={() => window.open(article.url, "_blank")} type="inner" hoverable="true"
-                        style={{ width: 380, marginBottom: 20 }}
-                        cover={article.urlToImage === "" || article.urlToImage === null ? null : 
-                        <img alt={article.title}
-                    src={article.urlToImage} /> 
+  return (
+    <div>
+      <Row>
+        <Col>
+          <h1 style={{ fontSize: "30px" }}>{request.topHeading}</h1>
+        </Col>
+      </Row>
+      <Row>
+        {newsSection.length > 1
+          ? newsSection.map((article, key) =>
+              article.urlToImage === "" ||
+              article.urlToImage === null ? null : (
+                <Col key={key} md={{ span: 8 }} sm={{ span: 24 }}>
+                  <Card
+                    onClick={() => window.open(article.url, "_blank")}
+                    type="inner"
+                    hoverable="true"
+                    style={{ width: 380, marginBottom: 20 }}
+                    cover={
+                      article.urlToImage === "" ||
+                      article.urlToImage === null ? null : (
+                        <img alt={article.title} src={article.urlToImage} />
+                      )
                     }
-                    title={article.source.name === "" || article.source.name === null ? null : "Source " + ReactHtmlParser(article.source.name)}
-                    extra={article.author === "" || article.author === null ? null : "Author: " + ReactHtmlParser(article.author)}
-                    >
-                        <h3>{ReactHtmlParser(article.title)}</h3>
-                        <Meta 
-                        description={ReactHtmlParser(article.description)} />
-                        </Card>
-                    </Col>
-                    )    
-                    : "loading.."
-            }
-            </Row>
-            {request.linkText != null?
-            <Row>
-                <Col>
-                <Divider/>
+                    title={
+                      article.source.name === "" || article.source.name === null
+                        ? null
+                        : "Source " + ReactHtmlParser(article.source.name)
+                    }
+                    extra={
+                      article.author === "" || article.author === null
+                        ? null
+                        : "Author: " + ReactHtmlParser(article.author)
+                    }
+                  >
+                    <h3>{ReactHtmlParser(article.title)}</h3>
+                    <Meta description={ReactHtmlParser(article.description)} />
+                  </Card>
                 </Col>
-            </Row> : null
-        }
-        </div>
-    )
-}
+              )
+            )
+          : "loading.."}
+      </Row>
+      {request.linkText != null ? (
+        <Row>
+          <Col>
+            <Divider />
+          </Col>
+        </Row>
+      ) : null}
+    </div>
+  );
+};
 
 export default NewsSection;
