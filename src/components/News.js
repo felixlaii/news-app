718e727d22494f460ce7a73fb08e2544eb762273
Felix Lai <felix.lai@hotmail.com>
2022-11-29 10:12:00 -0500

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -6,6 +6,8 @@ const News = () => {
     return (
         <div>
             <NewsSection category='top-headlines' query='country=in' topHeading='Top News' linkText='See More Headlines' results='15' />
+            <NewsSection category='everything' query='q=trending india' topHeading='Trending in India' linkText='More Indian Trends' results='12' />
+            <NewsSection category='everything' query='q=trending tech' topHeading='Tech Trends' linkText='More about Tech' results='12' />
         </div>
     )
 }
