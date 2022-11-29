b2ef858e17590207da38cdd4ef0cf653fb7e2459
Felix Lai <felix.lai@hotmail.com>
2022-11-29 10:10:59 -0500

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -5,7 +5,7 @@ import NewsSection from './NewsSection';
 const News = () => {
     return (
         <div>
-            
+            <NewsSection category='top-headlines' query='country=in' topHeading='Top News' linkText='See More Headlines' results='15' />
         </div>
     )
 }
