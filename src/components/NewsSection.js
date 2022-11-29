f21dfd6a7bd35f7cc5b94e8237f36df307a480fe
Felix Lai <felix.lai@hotmail.com>
2022-11-29 09:50:54 -0500

    Update NewsSection.js

--- src/components/NewsSection.js
+++ src/components/NewsSection.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import 'antd/dist/antd.css';
 import { Row, Col, Divider, Card } from 'antd';
-import { fetchNews } from '../api';
+import { fetchNews } from '../api/newsAPI';
 import ReactHtmlParser from 'react-html-parser';
 
 const NewsSection = (request) => {
     const [newsSection, setNewsSection] = useState([]);
-    useEffect(( => {
+    useEffect(() => {
         const fetchAPI = async () => {
             setNewsSection(await fetchNews(request));
-        }
-    })
+        };
+        fetchAPI();
+    }, [request])
 }
 
 export default NewsSection;
