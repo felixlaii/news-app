b06ca12ab028e49eeebf8746c603c22b6a2b4db1
Felix Lai <felix.lai@hotmail.com>
2022-11-29 10:14:55 -0500

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,3 +1,6 @@
 import React, { Suspense } from 'react';
 import 'antd/dist/antd.css';
 import { fetchSources } from '../api/newsAPI';
+
+const NewsSection = React.lazy(() => import('./NewsSection'));
+const News = React.lazy(() => import('./News'));
